8710ec45377bbaac22d634c48e1b37bdd40933dc
Rahul Tarak <cryogenicplanet@gmail.com>
2024-05-13 15:48:55 -0700

    seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import { type AppType } from "next/app";
 import { Inter } from "next/font/google";
 
+import { DefaultSeo } from "next-seo";
+
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
@@ -14,6 +16,23 @@ const inter = Inter({
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <main className={`font-sans ${inter.variable}`}>
+      <DefaultSeo
+        title="bench.audio"
+        description="bench.audio is the battle ground for voice agents"
+        openGraph={{
+          type: "website",
+          url: "https://bench.audio",
+          images: [
+            {
+              url: "https://bench.audio/hero.png",
+            },
+          ],
+        }}
+        twitter={{
+          handle: "@sfvoicecompany",
+          cardType: "summary_large_image",
+        }}
+      />
       <Toaster position="top-right" />
       <Component {...pageProps} />
     </main>
